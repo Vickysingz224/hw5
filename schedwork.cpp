bf093e78e9f15eb7932bfe4c899567647cbc3857
Victoria Singh <vrsingh@usc.edu>
2025-04-21 05:08:16 +0000

    Coding attempt

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,13 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelp(const AvailabilityMatrix& avail,
+ const size_t dailyNeed, 
+ const size_t maxShifts,
+ DailySchedule& sched,
+ std::vector<size_t>& workTime,
+ size_t day,
+ size_t position);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +44,93 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // Declaration of two variables to store the days and workers
+    size_t days = avail.size();
+    size_t workers = avail[0].size();
+
+    sched.resize(days, std::vector<Worker_T>(dailyNeed, INVALID_ID));
+    
+    // Variable to store the amount of shifts each worker is given
+    std::vector<size_t> workTime(workers,0);
+
+    return scheduleHelp(avail, dailyNeed, maxShifts, sched, workTime, 0, 0);
+
 
 
 
 }
+
+bool scheduleHelp(const AvailabilityMatrix& avail,
+ const size_t dailyNeed, 
+ const size_t maxShifts,
+ DailySchedule& sched,
+ std::vector<size_t>& workTime,
+ size_t day,
+ size_t position)
+ {
+    // // Declaration of two variables to store the days and workers
+    size_t days2 = avail.size();
+    size_t workers2 = avail[0].size();
+
+    //Check if the the day variable is full and the days are all scheduled
+    if(day == days2)
+    {
+        return true;
+    }
+
+    // Check if the position for workers are fileld for the day, move to next day
+    if(position == dailyNeed)
+    {
+         return scheduleHelp(avail, dailyNeed, maxShifts, sched, workTime, day+1, 0);
+    }
+
+    // Iterate through the group of workers and check to see if the worker can fill the position on the current day
+    for(Worker_T m =0; m < workers2; m++)
+    {
+        // Check if the worker is not available on day
+        if(!avail[day][m])
+        {
+            continue;
+        }
+         
+         // Check if worker has reached limit of shifts
+        if(workTime[m] >= maxShifts)
+        {
+            continue;
+        }
+        
+        // Loops through the schedule to see if the worker is scheduled 
+        bool checkSched = false;
+        for(size_t n=0; n < position; n++)
+        {
+            if(sched[day][n] == m)
+            {
+                checkSched = true;
+                break;
+            }
+        }
+         
+         // Check if worker is scheduled for day
+        if(checkSched)
+        {
+            continue;
+        }
+        
+        // Sets worker to position and increment their shifts
+        sched[day][position] = m;
+        workTime[m]++;
+
+        // recursivley schedule remaining positions
+        if(scheduleHelp(avail, dailyNeed, maxShifts, sched, workTime, day, position+1))
+        {
+            return true;
+        }
+      
+        // Backtracking 
+        sched[day][position] = INVALID_ID;
+        workTime[m]--;
+    }
+    return false;
+
+ }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordHelp( string& inputPattern, int posNum, string floatingNum, const set<string>& dict, set<string>& words);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,98 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    // Declaration of results stored in set of words and an input string
+    set<string> words;
+
+    string inputPattern = in;
+    // calls the recursive solution for the problem statement
+    wordHelp(inputPattern,0,floating,dict,words);
+     
+    return words;
 
 }
 
 // Define any helper functions here
+void wordHelp( string& inputPattern, int posNum, string floatingNum, const set<string>& dict, set<string>& words)
+{
+
+  // checks if the entire pattern is reached 
+  if(posNum == (int)inputPattern.size())
+  {
+     //Checks if the floating characters are used and that the word exists in the dictionary
+    if(floatingNum.empty() && dict.find(inputPattern) != dict.end())
+    {
+       // add it to set
+      words.insert(inputPattern);
+    }
+    return;
+  }
+
+
+  // For loop for iterating through remaining spaces and keeping a count
+  int spaces =0;
+  for(size_t m = posNum; m < inputPattern.size(); m++)
+  {
+    if(inputPattern[m] == '-')
+    {
+      spaces++;
+    }
+
+  }
+
+  // Checks if there are more floating characters than spaces
+  if(floatingNum.size() > (size_t)spaces)
+  {
+    return;
+  }
+  
+  // Checks if the current position already has character
+  if(inputPattern[posNum] != '-')
+  {
+     // recrusively go to next position
+    wordHelp(inputPattern, posNum+1, floatingNum, dict, words);
+    return;
+  }
+
+  // for loop that iterates through and attempts to place each floating character , and go to the next position
+  for(size_t n=0; n < floatingNum.size(); n++)
+  {
+   
+   char float1 = floatingNum[n];
+   inputPattern[posNum] = float1;
+   // remove the current floating character from set
+   string float2 = floatingNum;
+   float2.erase(n,1);
+   // Recursive call
+   wordHelp(inputPattern, posNum + 1, float2, dict, words);
+
+   inputPattern[posNum] = '-';
+  }
+
+  // Checks if there are more spaces than floating characters
+  if((size_t)spaces > floatingNum.size())
+  {
+    // Loops through each letter of the alphabet
+    for( char b = 'a'; b <='z'; b++)
+    {
+     bool isFloat = false;
+     for( char c: floatingNum)
+     {
+       if (b == c)
+       {
+          isFloat = true;
+          break;
+       }
+     }
+     // checks if letters are not in floating set, and calls recursive function to fo to next psoition
+     if(!isFloat)
+     { 
+        inputPattern[posNum]= b;
+        wordHelp(inputPattern, posNum +1, floatingNum, dict, words);
+        inputPattern[posNum] = '-';
+     }
+    }
+  }
+
+}
+
